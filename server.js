0e0ccd2d6272c44721fec2d5982bf2cde6c1459d
Gustavo Lima <gustavo.lima@pumpkin.care>
2022-03-31 21:31:42 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const PORT = process.env.PORT || 5000;
+
+const server = require('./src/app')({
+  logger: {
+    level: 'info',
+    prettyPrint: true,
+  },
+});
+
+(async () => {
+  try {
+    await server.listen(PORT);
+  } catch (error) {
+    server.log.error(error);
+    process.exit(1);
+  }
+})();
